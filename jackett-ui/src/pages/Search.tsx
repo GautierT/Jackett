dfbb57d6c6cd0baf101497e1f9097a0d0eb3010a
ngosang <ngosang@hotmail.es>
2020-04-12 05:01:07 +0200

    add more functionality to search

--- jackett-ui/src/pages/Search.tsx
+++ jackett-ui/src/pages/Search.tsx
@@ -1,14 +1,67 @@
 import React from 'react';
 import {connect} from "react-redux";
 import {RootState} from "../store/reducers";
-import {IndexerConfig, IndexersConfig} from "../store/types/indexersConfig";
-import {Card, Select, Table, Form, Input, Button } from 'antd';
+import {IndexersConfig} from "../store/types/indexersConfig";
+import {Card, Select, Table, Form, Input, Button, Tag} from 'antd';
 import {Store} from 'rc-field-form/lib/interface.d'
 import filesize from "filesize";
+import moment from "moment";
+import {ColumnsType} from "antd/lib/table/interface";
+import MagnetIcon from "../assets/magnet.svg";
+import DownloadIcon from "../assets/download.svg";
+import UploadIcon from "../assets/upload.svg";
+import "./Search.css";
+
+// TODO: move interfaces y api a otro lado (documentar campos)
+interface SearchResult {
+    BannerUrl: string
+    BlackholeLink: string
+    Category: Array<number>
+    CategoryDesc: string
+    Comments: string
+    Description: string
+    DownloadVolumeFactor: number
+    Files: number
+    FirstSeen: string
+    Gain: number
+    Grabs: number
+    Guid: string
+    Imdb: number
+    InfoHash: string
+    Link: string
+    MagnetUri: string
+    MinimumRatio: number
+    MinimumSeedTime: number
+    Peers: number
+    PublishDate: string
+    RageID: number
+    Seeders: number
+    Size: number
+    Title: string
+    TMDb: number
+    Tracker: string
+    TrackerId: string
+    TVDBId: number
+    UploadVolumeFactor: number
+}
+
+interface SearchIndexer {
+    ID: string
+    Name: string
+    Status: number
+    Results: number
+    Error: string
+}
+
+interface SearchResponse {
+    Results: Array<SearchResult>
+    Indexers: Array<SearchIndexer>
+}
+
 
 // TODO: add props & state
 interface State {
-    dataTable: Array<IndexerConfig>
+    searchResponse: SearchResponse
 }
 
 interface Props {
@@ -28,10 +81,10 @@ const mapDispatchToProps = {
 
 class Search extends React.Component<Props, State> {
 
-    constructor(props: any) {
+    constructor(props: Props) {
         super(props);
         this.state = {
-            dataTable: []
+            searchResponse: {} as SearchResponse
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -74,97 +127,169 @@ class Search extends React.Component<Props, State> {
         fetch("/api/v2.0/indexers/all/results?" + this.encodeQueryData(qc))
             .then(res => res.json())
             .then((data) => {
-                this.setState({ dataTable: data.Results })
+                this.setState({ searchResponse: data })
             })
             .catch(console.error)
 
     }
 
+    jackettTimespan(date: string) {
+        const now = moment();
+        const from = moment(date);
+        const timeSpan = moment.duration(now.diff(from));
+
+        const minutes = timeSpan.asMinutes();
+        if (minutes < 120) {
+            return Math.round(minutes) + 'm ago';
+        }
+
+        const hours = timeSpan.asHours();
+        if (hours < 48) {
+            return Math.round(hours) + 'h ago';
+        }
+
+        const days = timeSpan.asDays();
+        if (days < 365) {
+            return Math.round(days) + 'd ago';
+        }
+
+        const years = timeSpan.asYears();
+        return Math.round(years) + 'y ago';
+    }
+
+    downloadLinks(record: SearchResult) {
+        let torrent = record.Link ? <a href={record.Link}><img src={DownloadIcon} alt="Download torrent"/></a> : '';
+        let magnet = record.MagnetUri ? <a href={record.MagnetUri}><img src={MagnetIcon} alt="Download magnet"/></a> : '';
+        let blackhole = record.BlackholeLink ? <a href={record.BlackholeLink}><img src={UploadIcon} alt="Save in Blackhole directory"/></a> : '';
+
+        return (
+            <span className="download-icons">{torrent} {magnet} {blackhole}</span>
+        );
+    }
+
+    // TODO: add banner & description tooltip
+    // TODO: add tvrage y otros valores que no están en jackett ?
+    releaseTags(record: SearchResult) {
+        let labels = [];
+/*
+        var TitleTooltip = "";
+        if (Banner)
+            TitleTooltip += "<img src='" + Banner + "' /><br />";
+        if (Description)
+            TitleTooltip += Description;
+
+        if (TitleTooltip) {
+            TitleLink.data("toggle", "tooltip");
+            TitleLink.tooltip({
+                title: TitleTooltip,
+                html: true
+            });
+        }
+*/
+
+        if (record.DownloadVolumeFactor === 0) {
+            labels.push(<Tag color="green">FREELEECH</Tag>);
+        } else if (record.DownloadVolumeFactor < 1) {
+            labels.push(<Tag color="geekblue">{(record.DownloadVolumeFactor * 100).toFixed(0) + " % DL"}</Tag>);
+        } else if (record.DownloadVolumeFactor > 1) {
+            labels.push(<Tag color="red">{(record.DownloadVolumeFactor * 100).toFixed(0) + " % DL"}</Tag>);
+        }
+
+        if (record.UploadVolumeFactor === 0) {
+            labels.push(<Tag color="volcano">NO UPLOAD</Tag>);
+        } else if (record.UploadVolumeFactor != 1) {
+            labels.push(<Tag color="blue">{(record.UploadVolumeFactor * 100).toFixed(0) + " % UL"}</Tag>);
+        }
+
+        if (record.Imdb) {
+            const imdbUrl = "http://www.imdb.com/title/tt" + ("0000000" + record.Imdb).slice(-8) + "/";
+            labels.push(<Tag color="gold"><a href={imdbUrl} title="IMDB" target="_blank" rel="noopener noreferrer">IMDB</a></Tag>);
+        }
+
+        return (
+            [labels]
+        )
+    }
+
     render() {
-        const columns = [
+        const columns: ColumnsType<any> = [
             {
                 title: 'Published',
                 dataIndex: 'PublishDate',
-                searchable: false,
                 width: '1px',
-                sorter: (a:any, b:any) => a.PublishDate.localeCompare(b.PublishDate)
+                defaultSortOrder: 'ascend',
+                sorter: (a:SearchResult, b:SearchResult) => b.PublishDate.localeCompare(a.PublishDate),
+                render: (text:string, record:SearchResult) => <span style={{whiteSpace: "nowrap"}}>{this.jackettTimespan(record.PublishDate)}</span>
             },
             {
                 title: 'Tracker',
                 dataIndex: 'Tracker',
-                searchable: false,
                 width: '1px',
-                sorter: (a:any, b:any) => a.Tracker.localeCompare(b.Tracker)
+                sorter: (a:SearchResult, b:SearchResult) => a.Tracker.localeCompare(b.Tracker),
+                render: (text:string, record:SearchResult) => <span style={{whiteSpace: "nowrap"}}>{record.Tracker}</span>
+            },
+            {
+                title: 'DL',
+                dataIndex: 'UploadVolumeFactor',
+                width: '1px',
+                render: (text:string, record:SearchResult) => this.downloadLinks(record)
             },
             {
                 title: 'Name',
                 dataIndex: 'Title',
                 width: '100%',
-                sorter: (a:any, b:any) => a.Title.localeCompare(b.Title)
-                // render: rowData => <a href={rowData.Comments} target="_blank">{ rowData.Title }</a>
+                sorter: (a:SearchResult, b:SearchResult) => a.Title.localeCompare(b.Title),
+                render: (text:string, record:SearchResult) => <span><a href={record.Comments} target="_blank" rel="noopener noreferrer">{record.Title}</a> {this.releaseTags(record)}</span>
             },
             {
                 title: 'Size',
                 dataIndex: 'Size',
-                searchable: false,
                 width: '1px',
-                sorter: (a:any, b:any) => a.Size - b.Size,
-                render: (text:any, record:any, index:any) => <span style={{ whiteSpace: "nowrap" }}>{  filesize(text) }</span>
+                sorter: (a:SearchResult, b:SearchResult) => a.Size - b.Size,
+                render: (text:string, record:SearchResult) => <span style={{whiteSpace: "nowrap"}}>{filesize(record.Size)}</span>
             },
             {
                 title: 'Files',
                 dataIndex: 'Files',
-                searchable: false,
                 width: '1px',
-                sorter: (a:any, b:any) => a.Files - b.Files
+                sorter: (a:SearchResult, b:SearchResult) => a.Files - b.Files
             },
             {
                 title: 'Category',
                 dataIndex: 'CategoryDesc',
                 width: '1px',
-                sorter: (a:any, b:any) => a.CategoryDesc.localeCompare(b.CategoryDesc)
+                sorter: (a:SearchResult, b:SearchResult) => a.CategoryDesc.localeCompare(b.CategoryDesc),
+                render: (text:string, record:SearchResult) => <span style={{whiteSpace: "nowrap"}}>{record.CategoryDesc}</span>
             },
             {
                 title: 'Grabs',
                 dataIndex: 'Grabs',
-                searchable: false,
                 width: '1px',
-                sorter: (a:any, b:any) => a.Grabs - b.Grabs
+                sorter: (a:SearchResult, b:SearchResult) => a.Grabs - b.Grabs
             },
             {
                 title: 'Seeds',
                 dataIndex: 'Seeders',
-                searchable: false,
                 width: '1px',
-                sorter: (a:any, b:any) => a.Seeders - b.Seeders
+                sorter: (a:SearchResult, b:SearchResult) => a.Seeders - b.Seeders
             },
             {
                 title: 'Leechers',
                 dataIndex: 'Peers',
-                searchable: false,
                 width: '1px',
-                sorter: (a:any, b:any) => a.Peers - b.Peers
+                sorter: (a:SearchResult, b:SearchResult) => a.Peers - b.Peers
             },
             {
                 title: 'DF',
                 dataIndex: 'DownloadVolumeFactor',
-                searchable: false,
                 width: '1px',
-                sorter: (a:any, b:any) => a.DownloadVolumeFactor - b.DownloadVolumeFactor
+                sorter: (a:SearchResult, b:SearchResult) => a.DownloadVolumeFactor - b.DownloadVolumeFactor
             },
             {
                 title: 'UF',
                 dataIndex: 'UploadVolumeFactor',
-                searchable: false,
                 width: '1px',
-                sorter: (a:any, b:any) => a.UploadVolumeFactor - b.UploadVolumeFactor
-            },
-            {
-                title: 'DL',
-                dataIndex: 'UploadVolumeFactor',
-                searchable: false,
-                width: '1px',
-                //render: rowData => this.downloadLinks(rowData)
+                sorter: (a:SearchResult, b:SearchResult) => a.UploadVolumeFactor - b.UploadVolumeFactor
             }
         ];
 
@@ -176,7 +301,7 @@ class Search extends React.Component<Props, State> {
 
 
         return (
-            <Card title="Search" extra={<a href="#">More</a>} style={{ width: "100%" }}>
+            <Card title="Search" style={{ width: "100%" }}>
                 <div>
                     <Form
                         layout="inline"
@@ -209,20 +334,17 @@ class Search extends React.Component<Props, State> {
                         </Form.Item>
                     </Form>
                 </div>
-                <Table dataSource={this.state.dataTable} columns={columns} size="small" pagination={{position:["bottomLeft"]}}/>
+                <Table
+                    dataSource={this.state.searchResponse.Results}
+                    columns={columns}
+                    size="small"
+                    pagination={{position:["bottomLeft"]}}
+                    showSorterTooltip={false}
+                />
             </Card>
         );
     }
-/*
-    downloadLinks(rawData: any) {
-        let torrent = rawData.Link ? <a class="downloadlink" title="Download locally" target="_blank" href={rawData.Link}>Down</a> : '';
-        let magnet = rawData.MagnetUri ? <a class="downloadlink" title="Download locally (magnet)" target="_blank" href={rawData.MagnetUri}>Magnet</a> : '';
-        let blackhole = rawData.BlackholeLink ? <a class="downloadlink jacketdownloadserver" title="Save to server blackhole directory" target="_blank" href={rawData.BlackholeLink}>Blackhole</a> : '';
 
-        return (
-            <span>{torrent} {magnet} {blackhole}</span>
-        );
-    }*/
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
