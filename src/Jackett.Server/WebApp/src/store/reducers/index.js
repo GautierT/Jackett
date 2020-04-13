1e229b9e7e2d06a052c665bad12243e39b182b42
ngosang <ngosang@hotmail.es>
2020-04-13 20:09:03 +0200

    move files inside jackett server

--- src/Jackett.Server/WebApp/src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import configReducer from './config';
-import productsReducer from './indexers';
-
-export default combineReducers({
-  config: configReducer,
-  indexers: productsReducer
-});
